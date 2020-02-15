157be49641e70f04c5b5b5ffb561788f8694d70d
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-02-15 23:41:56 +0100

    Dodal viewe za teste.
    * index: seznam testov,
    * podrobnosti: podatki o testu,
    * nadloga: nadloga.

--- nadlogar/config/urls.py
+++ nadlogar/config/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('naloge/', include('naloge.urls')),
+    path('testi/', include('testi.urls')),
 ]
