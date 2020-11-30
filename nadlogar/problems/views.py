2a39296a472871a593c6beb466ef827b003a0fe4
Matija Pretnar <matija@pretnar.info>
2020-11-30 09:57:14 +0000

    V vmesnih korakih uporabljal GET, kjer se da

--- nadlogar/problems/views.py
+++ nadlogar/problems/views.py
@@ -1,45 +1,45 @@
-from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import get_object_or_404, redirect, render
-from quizzes.models import Quiz
 
 from .forms import ProblemForm, problem_parameters_form, problem_text_form
 from .models import Problem
 
 
 def create(request):
-    problem_form = ProblemForm(request.POST or None)
+    problem_form = ProblemForm(request.POST or request.GET or None)
     if problem_form.is_valid():
         content_type = problem_form.cleaned_data["content_type"]
         model_class = content_type.model_class()
         return create_parameters(request, model_class)
     return render(
         request,
-        "problems/choose_generator.html",
+        "problems/create_generator.html",
         {"form": problem_form},
     )
 
 
 def create_parameters(request, model_class):
-    parameters_form = problem_parameters_form(model_class, request.POST or None)
+    parameters_form = problem_parameters_form(
+        model_class, request.POST or request.GET or None
+    )
     if parameters_form.is_valid():
         return create_text(request, model_class)
     return render(
         request,
-        "problems/choose_parameters.html",
+        "problems/create_parameters.html",
         {"form": parameters_form},
     )
 
 
 def create_text(request, model_class):
     text_form = problem_text_form(
-        model_class, request.POST or None, initial={"text": None}
+        model_class, request.POST or request.GET or None, initial={"text": None}
     )
-    if text_form.is_valid():
+    if request.method == "POST" and text_form.is_valid():
         problem = text_form.save()
         return redirect("quizzes:details", quiz_id=problem.quiz.id)
     return render(
         request,
-        "problems/choose_text.html",
+        "problems/create_text.html",
         {"form": text_form},
     )
 
@@ -59,10 +59,10 @@ def edit_parameters(request, problem_id: int):
     form = problem_parameters_form(
         type(problem.downcast()), request.POST or None, instance=problem
     )
-    if form.is_valid():
+    if request.method == "POST" and form.is_valid():
         problem: Problem = form.save()
         return redirect("quizzes:details", quiz_id=problem.quiz.id)
-    return render(request, "problems/choose_parameters.html", {"form": form})
+    return render(request, "problems/edit_parameters.html", {"form": form})
 
 
 def edit_text(request, problem_id: int):
@@ -70,10 +70,10 @@ def edit_text(request, problem_id: int):
     form = problem_text_form(
         type(problem.downcast()), request.POST or None, instance=problem
     )
-    if form.is_valid():
+    if request.method == "POST" and form.is_valid():
         problem: Problem = form.save()
         return redirect("quizzes:details", quiz_id=problem.quiz.id)
-    return render(request, "problems/choose_text.html", {"form": form})
+    return render(request, "problems/edit_text.html", {"form": form})
 
 
 def delete(request, problem_id: int):
