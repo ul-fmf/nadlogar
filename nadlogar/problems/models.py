49567e2d6fa97c0b37d5c66959052c3874af9840
LenartBucar <lenart.bucar@student.fmf.uni-lj.si>
2021-08-12 21:25:47 +0200

    Dodan generator za naloge potenciranja veččlenika

--- nadlogar/problems/models.py
+++ nadlogar/problems/models.py
@@ -460,3 +460,54 @@ class PotencaTroclenika(Problem):
             "izraz": sympy.latex(izraz),
             "resitev": sympy.latex(sympy.expand(izraz)),
         }
+
+
+class PotencaVecclenika(Problem):
+    """Problem za potenciranje veččlenika."""
+
+    najmanjsa_potenca = models.PositiveSmallIntegerField(
+        "najmanjša potenca", help_text="Najmanjša možna potenca veččlenika."
+    )
+    najvecja_potenca = models.PositiveSmallIntegerField(
+        "največja potenca", help_text="Največja možna potenca veččlenika."
+    )
+    najmanj_clenov = models.PositiveSmallIntegerField(
+        "najmanj členov", help_text="Najmanjše možno število členov v veččleniku."
+    )
+    najvec_clenov = models.PositiveSmallIntegerField(
+        "največ členov", help_text="Največje možno število členov v veččleniku."
+    )
+    linearna_kombinacija = models.BooleanField(
+        "linearna kombinacija",
+        help_text="Ali naj naloga vsebuje linearno kombinacijo neznank ali enostaven veččlenik?",
+    )
+
+    class Meta:
+        verbose_name = "potenciranje veččlenika"
+
+    def generate(self):
+        potenca = random.randint(self.najmanjsa_potenca, self.najvecja_potenca)
+        cleni = random.randint(self.najmanj_clenov, self.najvec_clenov)
+        simboli = [
+            sympy.symbols(chr(x)) for x in random.sample(range(97, 123), cleni)
+        ]  # izberemo naključne znake abecede
+        if potenca == 2:
+            do = 10
+        else:
+            do = 5
+
+        koeficienti = random.choices([x for x in range(-do, do) if x != 0], k=cleni)
+        if not self.linearna_kombinacija:
+            potence = [1 for _ in range(cleni)]
+        else:
+            potence = random.choices(range(1, 4), k=cleni)
+
+        vrednosti = zip(koeficienti, simboli, potence)
+        izraz = sympy.Pow(
+            sum(k * s ** p for k, s, p in vrednosti), potenca, evaluate=False
+        )
+
+        return {
+            "izraz": sympy.latex(izraz),
+            "resitev": sympy.latex(sympy.expand(izraz)),
+        }
