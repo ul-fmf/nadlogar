b21ca3fff58dd0340028dded861dfbccdff73f0b
Matija Pretnar <matija@pretnar.info>
2021-08-19 19:59:49 +0000

    Dodal aplikacijo za uporabnike

--- nadlogar/config/settings.py
+++ nadlogar/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "utils",
+    "users",
     "students",
     "quizzes",
     "problems",
@@ -105,6 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "users.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
