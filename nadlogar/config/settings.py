f2a8b18445d4a4a3610919c55caf0a8ec137a91a
Matija Pretnar <matija@pretnar.info>
2021-09-20 11:01:40 +0000

    Drugačna vsebina začetne strani

--- nadlogar/config/settings.py
+++ nadlogar/config/settings.py
@@ -67,7 +67,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "students.context_processors.my_groups",
             ],
         },
     },
--- nadlogar/utils/views.py
+++ nadlogar/utils/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from documents.models import Document
+from students.models import StudentGroup
 
 
 @login_required
 def homepage(request):
     documents = Document.objects.filter(student_group__user=request.user)
-    return render(request, "homepage.html", {"documents": documents})
+    groups = StudentGroup.objects.filter(user=request.user)
+    return render(request, "homepage.html", {"documents": documents, "groups": groups})
